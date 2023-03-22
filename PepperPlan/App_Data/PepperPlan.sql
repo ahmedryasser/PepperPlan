613a5b3c457d83823891c6f6701101bd46b36a6a
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2023-03-21 18:50:08 -0700

    Add project files.

---
+++ PepperPlan/App_Data/PepperPlan.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ClassId] NVARCHAR(36) NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(36) NULL, 
+    [Prerequisites] NVARCHAR(36) NULL, 
+    [GesSatisfied] NVARCHAR(36) NULL, 
+    [MajorSatisfied] NVARCHAR(36) NULL, 
+    [MinorSatisfied] NVARCHAR(36) NULL, 
+    [Units] INT NULL, 
+    [SemestersOffered] NVARCHAR(36) NULL
+)
