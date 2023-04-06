5f13cdefc9abf2732f98a02c0dd5ddd79670e60d
Joebe44 <joeuseful80@gmail.com>
2023-04-05 18:32:36 -0700

    Units inputs

---
+++ PepperPlan/Models/HomeDropdown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PepperPlan.Models
+{
+	public class HomeDropdown
+	{
+		public HomeDropdown()
+		{
+
+		}
+
+	}
+}
+
---
+++ PepperPlan/Models/MajorSelection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+namespace PepperPlan.Models
+{
+	public class MajorSelection
+	{
+        //public MajorSelection()
+        //{
+
+        //}
+        private SelectList _MajorSelectList { get; set; }
+
+		public SelectList MajorSelectList
+		{
+			get
+			{
+				if(_MajorSelectList != null)
+				{
+					return _MajorSelectList;
+					//returns list of Majors
+				}
+				return new SelectList(GetMajors(), "ID", "name");
+			}
+
+			set { _MajorSelectList = value; }
+		}
+        private List<MajorVM> GetMajors()
+        {
+            var majors = new List<MajorVM>()
+            majors.Add(new MajorVM() { ID = 1, name = "Business" });
+            majors.Add(new MajorVM() { ID = 2, name = "Computer Science" });
+            return majors;
+        }
+    }
+}
+
